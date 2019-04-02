4e145fa21c6b104de7ae728b64ecda8750806007
Easiv <easiv89@gmail.com>
2019-04-02 15:55:08 +0200

    by ember data

--- app/components/question-viewer/component.js
+++ app/components/question-viewer/component.js
@@ -1,11 +1,4 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  question: '',
-
-  didRender() {
-    fetch('http://localhost:3000/questions')
-    .then(res => res.json())
-    .then(res => this.set('question', res.name))
-  }
 });
