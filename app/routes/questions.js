d6d8bfd0a4264fd70429b5b9f7141cff9b92370d
Easiv <easiv89@gmail.com>
2019-04-03 16:05:08 +0200

    rest adapter and serializer

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
+    console.log(this.store.findAll('question'))
     return this.store.findAll('question');
   } 
 });
