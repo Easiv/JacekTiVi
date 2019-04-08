805466749fdc4ea1ebb8b9ae6b33dcb6363fe745
Easiv <easiv89@gmail.com>
2019-04-08 20:23:04 +0200

    update n delete

--- app/components/question-viewer/component.js
+++ app/components/question-viewer/component.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  store: service(),
+
+  actions: {
+    deleteQuestion(question) {
+      if(confirm('Are you sure?')) {
+        this.get('store').findRecord('question', question.id, {reload: true}).then(q => q.destroyRecord())
+      }
+    },
+
+    updateQuestion(question) {
+      this.get('store').findRecord('question', question.id).then(q => {
+        q.set('name', 'abc');
+        q.save()
+      })
+
+    }
+  }
 });
