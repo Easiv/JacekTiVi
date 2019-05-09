7bbff89793c2df3b2f132fb08c6c642bd1bf6c21
Easiv <easiv89@gmail.com>
2019-05-09 15:33:08 +0200

    userList with names, remove redundant stuff (again)

--- app/components/kwiplasz-game/component.js
+++ app/components/kwiplasz-game/component.js
@@ -1,12 +1,4 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  currentuser: service(),
-  you: '',
-
-  willRender() {
-    this._super(...arguments);
-    this.set('you', this.get('currentuser').getId());
-  }
 });
