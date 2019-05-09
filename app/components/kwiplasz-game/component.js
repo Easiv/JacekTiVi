3f7d025be5718adb7e6091765589fab7ef80dddd
Easiv <easiv89@gmail.com>
2019-05-09 15:14:36 +0200

    Revert "userList with names, remove redundant stuff"
    This reverts commit f1f9164c6ab8a99b0d34c5159f1527d514c535d1.

--- app/components/kwiplasz-game/component.js
+++ app/components/kwiplasz-game/component.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  currentuser: service(),
+  you: '',
+
+  willRender() {
+    this._super(...arguments);
+    this.set('you', this.get('currentuser').getId());
+  }
 });
