6ee98364e8b470c77fc770c00cde0c72bacfc6a8
Easiv <easiv89@gmail.com>
2019-05-13 00:45:09 +0200

    loading room-users on init, improve owner display

--- app/components/kwiplasz-game/component.js
+++ app/components/kwiplasz-game/component.js
@@ -1,6 +1,20 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  store: service(),
+  roomId: null,
+  userList: null,
+
+  init() {
+    this._super(...arguments);
+
+    this.store.findAll('user')
+    .then(users => {
+      let userList = users.filterBy('roomId', this.get('roomId'));
+      this.set('userList', userList)
+    });
+  },
 
   actions: {
     success() {},
