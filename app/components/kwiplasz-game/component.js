ecb0895266c393fc4cf438c99cdd4e7824505c38
Easiv <easiv89@gmail.com>
2019-05-20 20:38:47 +0200

    randoming 3 questions

--- app/components/kwiplasz-game/component.js
+++ app/components/kwiplasz-game/component.js
@@ -6,7 +6,7 @@ export default Component.extend({
   hasStarted: false,
   roomId: null,
   userList: null,
-  question: null,
+  questions: [],
   countdown: null,
 
   init() {
@@ -38,23 +38,51 @@ export default Component.extend({
           alert('Koniec czasu!');
           clearInterval(timer);
           return null;
-          //gdy nie wpisal ktos odpowiedzi wyslij ze nie wpisal
-          // i tak czy siak
         }
       }, 1000);
     });
   },
 
+  randomThree(questions) {
+    //questions = questions.rejectBy('id', '5ce2ec4e89278f0cdf8fd817');
+
+    let maxInt = questions.length;
+    let previousRandoms = [];
+    
+    for(let i = 0; i < 3; i++) {
+      let random = Math.floor(Math.random() * maxInt);
+
+      if(previousRandoms.includes(random)) {
+        i--;
+      } else {
+        this.get('questions').pushObject(questions[random]);
+      }
+      previousRandoms.push(random);
+    }
+  },
+
   actions: {
     startGame() {
-      this.get('store').queryRecord('question', {}).then(question => {
-        this.set('question', question);
-        this.get('store').findRecord('room', this.get('roomId')).then(room => {
-          room.set('hasStarted', true);
-          room.save();
-          this.writingTime();
+      this.get('store').findRecord('room', this.get('roomId')).then(room => {
+        this.get('store').findAll('question').then(questions => {
+          let used = room.usedQuestions;
+
+          if(used.length) {
+            used.forEach(q => {
+              questions = questions.rejectBy('id', q.id)
+            });
+            this.randomThree(questions);
+          } else {
+            questions = questions.rejectBy('id', null)
+            this.randomThree(questions);
+          }
+          this.get('questions').forEach(x => used.push(x.id));
         })
-      })
+        room.set('hasStarted', true);
+        room.save();
+        this.writingTime();
+      })  
+
       //6 pytan, bo na razie wersja na 3 osoby - pozniej ta pula rozdzielana jest posrod graczy
       // gdzie kazdy z nich konkuruje odpowiedzia, one rozdysponowane sa a - gracz, b - pytanie
       //  b1a1 : b1a2, b2a1 : b2a3, b3a2 : b3a3
