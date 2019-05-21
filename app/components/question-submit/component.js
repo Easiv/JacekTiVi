a0ebaa4fe1315ebe13865e3a478b9971a57aa895
Easiv <easiv89@gmail.com>
2019-05-21 20:20:22 +0200

    fixed missing outlet & transition after creating question

--- app/components/question-submit/component.js
+++ app/components/question-submit/component.js
@@ -3,12 +3,14 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   store: service(),
+  router: service(),
 
   actions: {
     submitQuestion(name) {
       this.get('store').createRecord('question', {
         name
       }).save()
+      this.get('router').transitionTo('questions');
     }
   }
 });
